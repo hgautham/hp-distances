0984e91f466467e007842278681cdd4a836e0334
gh045951 <gautham.hullikunte@cerner.com>
2020-11-24 18:15:03 -0600

    Hacking up a wizarding world travel calculator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState, useEffect} from 'react';
 
 function App() {
+
+  const [currentSum,setCurrentSum]= useState(0);
+  const [expressSum,setExpressSum]= useState(0);
+  const [clear,setClear]=useState(false);
+
+
+  const Broomify=(e)=>{
+    e.preventDefault();
+    if(clear) setClear(false);
+    let currentNum=document.querySelector('#num').value
+    if(currentNum=='')
+    return;
+    let sum= currentNum/150;
+    setCurrentSum(sum);
+    let expressSum= currentNum/35;
+    setExpressSum(expressSum);
+    document.querySelector('#num').value="";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          How fast can you travel in the wizarding world? <br/>
+
+          Enter a distance and click on Calculate to find out
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form>
+            <input type="text" id="num" placeholder="enter a distance" /><br />
+            <button onClick={Broomify}>Calculate</button><br /><br />
+            <button>Hogwarts Express</button>
+            <input type="text" id="result" value={currentSum}  readOnly /><br/>
+            <button>Broom</button>
+            <input type="text" id="result" value={expressSum}  readOnly />
+      </form>
+
+      <p>
+        FAQ: <br/>
+
+        Fastest ways to travel in the wizarding world <br/><br/>
+
+        Apparition - Near instant <br />
+        Broom - Firebolt speciality - 150 Miles/Hr <br/>
+        Hogwarts Express - 35 Miles/Hr
+      </p>
+
       </header>
     </div>
   );
